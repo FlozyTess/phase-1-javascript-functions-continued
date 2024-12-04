c627282d49078602b6d74fa2b7c8945888af31b1
FlozyTess <cecilrency23@gmail.com>
2024-12-04 14:17:26 +0300

    implemented a function that permits overriding default activity

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-function saturdayFun (activity=rollerskate){
+function saturdayFun (activity = "roller-skate"){
    return `This Saturday, I want to ${activity}!`
-}console.log (saturdayFun("roller skate"))
+}
+console.log (saturdayFun())
+function mondayWork (activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+console.log (mondayWork())
