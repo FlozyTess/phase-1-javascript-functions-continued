f221ff26f5eb0c3b88763cb8fdf13573dce4ff4b
FlozyTess <cecilrency23@gmail.com>
2024-12-04 14:45:17 +0300

    completed lab index.js

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ function mondayWork (activity = "go to the office"){
     return `This Monday, I will ${activity}.`
 }
 console.log (mondayWork())
+function wrapAdjective(flair = '*'){
+    return function (adjective ="special"){
+    return `You are ${flair}${adjective}${flair}!`
+    };
+}
+const wrapWithStars = wrapAdjective('*');
+console.log(wrapWithStars("a hard worker"));
+const wrapWithPipes = wrapAdjective('||');
+console.log(wrapWithPipes("a dedicated programmer"));
